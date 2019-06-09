6ce4a3f8fb5f3ecbfcc95b969a462da7c58ac699
alex chalyy <alexchalyy@gmail.com>
2019-06-09 15:30:08 -0400

    db schema set up

---
+++ models/user.js
@@ -0,0 +1,28 @@
+//  This creates a new table to store orders.
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("Users", {
+    username: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    password: DataTypes.STRING,
+    address: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    zip: DataTypes.INTEGER,
+    creditCardNumber: DataTypes.STRING,
+    expDate: DataTypes.STRING,
+    cvv: DataTypes.INTEGER,
+    swag1name: DataTypes.STRING,
+    swag1quantity: { type: DataTypes.INTEGER, defaultValue: 0 },
+    swag2name: DataTypes.STRING,
+    swag2quantity: { type: DataTypes.INTEGER, defaultValue: 0 },
+    swag3name: DataTypes.STRING,
+    swag3quantity: { type: DataTypes.INTEGER, defaultValue: 0 },
+    swag4name: DataTypes.STRING,
+    swag4quantity: { type: DataTypes.INTEGER, defaultValue: 0 },
+    swag5name: DataTypes.STRING,
+    swag5quantity: { type: DataTypes.INTEGER, defaultValue: 0 }
+  });
+  return User;
+};
