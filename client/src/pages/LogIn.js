50c58b31963c63ee99ec484fac7de1f48e09e62a
alex chalyy <alexchalyy@gmail.com>
2019-06-10 15:23:34 -0400

    log in page is not working

---
+++ client/src/pages/LogIn.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { isAbsolute } from "path";
+import Nav from "../components/Nav";
+
+console.log("before Log In function is called.");
+function LogIn() {
+    return (
+        <div className="App">
+            <Nav />
+            <p>GoT Swag?</p>
+        </div>
+    );
+}
+console.log("after Log In function is called.");
+
+export default LogIn;
