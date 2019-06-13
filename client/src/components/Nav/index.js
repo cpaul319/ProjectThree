2778985ee5ada3307a7927da783bb8dcc13a0798
Chris Paul <cpaul319@users.noreply.github.com>
2019-06-13 04:19:05 -0400

    minor changes, figuring out react still

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,16 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function Nav() {
   return (
     <nav className="navbar navbar-dark bg-dark">
-      <a className="navbar-brand" href="#">
-        Log In
-      </a>
-      <span>  </span>
-      <a className="navbar-brand" href="#">
-        Register
-      </a>
+       <Link to="/login">Log In</Link> | &nbsp;
+            <Link to="/enter">Register</Link>
+   
     </nav>
   );
 }
