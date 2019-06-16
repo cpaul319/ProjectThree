ab94929c49aaf8040b7546d06c4c1845d5064ee2
alex chalyy <alexchalyy@gmail.com>
2019-06-15 20:07:27 -0400

    forgot password text box is inserted

--- client/src/pages/Forgot.js
+++ client/src/pages/Forgot.js
@@ -2,16 +2,54 @@ import React, { Component } from "react";
 import ReactDOM from 'react-dom';
 import { isAbsolute } from "path";
 import Nav from "../components/Nav";
+import { Input, TextArea, FormBtn } from "../components/Form";
 
+class Forgot extends Component  {
+    state = {
+        email: ""
+    };
+
+    handleInputChange = event =>    {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        console.log("email is " + this.email);
+    }
+
+    render()    {
+        return(
+            <div className="App">
+                <Nav />
+                <p>Forgot password?</p>
+                <form>
+              <Input
+                value={this.state.title}
+                onChange={this.handleInputChange}
+                name = "email"
+                placeholder="email (required)"
+              />
+              <a href = '/sale'>Submit</a>
+            </form>
+            </div>
+        );
+    }
+}
+
+export default Forgot;
+
+/*
 function Forgot() {
     return (
         <div className="App">
             <Nav />
             <p>Forgot login page</p>
-            <p><a href = '/sale'>Submit</a></p>
         </div>
     );
 }
-console.log("after Log In function is called.");
 
-export default Forgot;
+export default Forgot; */
