02eda8cf4fbf2921450875f2a90ab2d51bac8b79
Chris Paul <cpaul319@users.noreply.github.com>
2019-06-18 20:03:49 -0400

    minor changes

--- controller/userController.js
+++ controller/userController.js
@@ -16,6 +16,24 @@ module.exports = {
             res.json(dbUser);
         });
     },
+     // app.post('/user', [
+  //   // username must be an email
+  //   check('username').isEmail(),
+  //   // password must be at least 5 chars long
+  //   check('password').isLength({ min: 5 })
+  // ], (req, res) => {
+  //   // Finds the validation errors in this request and wraps them in an object with handy functions
+  //   const errors = validationResult(req);
+  //   if (!errors.isEmpty()) {
+  //     return res.status(422).json({ errors: errors.array() });
+  //   }
+  
+  //   User.create({
+  //     username: req.body.username,
+  //     password: req.body.password
+  //   }).then(user => res.json(user));
+  // })0
+
     delete: function (req, res) {
         db.User.destroy({
             where: {
