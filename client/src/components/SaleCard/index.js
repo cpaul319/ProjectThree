ea433709bf573259a96f9c3c6e3cb01c575814ab
alex chalyy <alexchalyy@gmail.com>
2019-06-23 19:54:51 -0400

    image added

--- client/src/components/SaleCard/index.js
+++ client/src/components/SaleCard/index.js
@@ -74,7 +74,7 @@ class SaleCard extends Component {
     return (
       <div>
         <Card>
-          <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+          <CardImg top width="100%" src="hand.JPG" alt="Card image cap" />
           <CardBody>
             <CardTitle>Name</CardTitle>
             <CardSubtitle>Price</CardSubtitle>
