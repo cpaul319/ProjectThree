7a1d2562734ddc4ceca769a3a70e409e799a14e4
alex chalyy <alexchalyy@gmail.com>
2019-06-24 18:06:21 -0400

    log in page fixed a little

--- client/src/pages/Enter.js
+++ client/src/pages/Enter.js
@@ -10,44 +10,44 @@ import { Button } from 'reactstrap';
 import Axios from 'axios';
 
 class Enter extends Component {
-  state = { 
-    email: "",
-    password: "",
-    redirect: false
-  };
- 
-  setRedirect = () => {
-    this.setState({
-      redirect: true
-    })
-  }
+    state = {
+        email: "",
+        password: "",
+        redirect: false
+    };
 
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-    console.log("value is " + value);
-  }
-
-  renderRedirect = () => {
-    console.log("redirect works!");
-    if (this.state.redirect) {
-      return <Redirect to='/sale' />
+    setRedirect = () => {
+        this.setState({
+            redirect: true
+        })
     }
-  }
 
-  handleFormSubmit = event => {
-    console.log("submit!");
-    console.log("email: " + this.state.email);
-    console.log("password: " + this.state.password);
-    if (!this.state.email) {
-      console.log("e-mail empty");
-    } else {
-      this.setRedirect();
-      this.renderRedirect();
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+        console.log("value is " + value);
     }
-  }
+
+    renderRedirect = () => {
+        console.log("redirect works!");
+        if (this.state.redirect) {
+            return <Redirect to='/sale' />
+        }
+    }
+
+    handleFormSubmit = event => {
+        console.log("submit!");
+        console.log("email: " + this.state.email);
+        console.log("password: " + this.state.password);
+        if (!this.state.email) {
+            console.log("e-mail empty");
+        } else {
+            this.setRedirect();
+            this.renderRedirect();
+        }
+    }
 
     handleFormSubmit = event => {
 
@@ -82,23 +82,25 @@ class Enter extends Component {
         return (
             <div className="App">
                 <Nav />
-                 
-                <p>Login page</p>
-                {this.renderRedirect()}
-                <AvForm>
-                    <AvField name="email" label="Email" type="email" onChange={this.handleInputChange} validate={{
-                        email: { value: true, errorMessage: 'Please enter valid e-mail' },
-                        required: { value: true, errorMessage: 'Please enter e-mail' }
-                    }} />
-                    <AvField name="password" label="Password" type="text" onChange={this.handleInputChange} validate={{
-                        required: { value: true, errorMessage: 'Please enter password' },
-                        pattern: { value: '^[A-Za-z0-9]+$', errorMessage: 'Your name must be composed only with letter and numbers' },
 
-                        maxLength: { value: 16, errorMessage: 'Your name must be between 6 and 16 characters' }
-                    }} /> 
-                  
-                    <Button color="primary" onClick={this.handleFormSubmit}>Submit</Button>
-                  </AvForm>*/}
+                {this.renderRedirect()}
+                <div className='container'>
+                    <AvForm>
+                        <AvField name="email" label="Email" type="email" onChange={this.handleInputChange} validate={{
+                            email: { value: true, errorMessage: 'Please enter valid e-mail' },
+                            required: { value: true, errorMessage: 'Please enter e-mail' }
+                        }} />
+                        <AvField name="password" label="Password" type="text" onChange={this.handleInputChange} validate={{
+                            required: { value: true, errorMessage: 'Please enter password' },
+                            pattern: { value: '^[A-Za-z0-9]+$', errorMessage: 'Your name must be composed only with letter and numbers' },
+
+                            maxLength: { value: 16, errorMessage: 'Your name must be between 6 and 16 characters' }
+                        }} />
+
+                        <Button color="primary" onClick={this.handleFormSubmit}>Submit</Button>
+                    </AvForm>
+                </div>
+                {/*}
         <div className='container'>
           <form />
             <div className="form-group" />
@@ -110,10 +112,10 @@ class Enter extends Component {
               <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" onChange={this.handleInputChange} />
             <div class="form-group form-check" />
               <button type="submit" class="btn btn-primary" onClick={this.handleFormSubmit}>Submit</button>
-        </div>
-      </div>
-    )
-  }
+                </div>*/}
+            </div>
+        )
+    }
 }
 
 export default Enter;
