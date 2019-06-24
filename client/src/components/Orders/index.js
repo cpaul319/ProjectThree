5e025c01bc720108074342682fde47747d94fe5e
unknown <kevhemsoth@gmail.com>
2019-06-24 17:20:41 -0400

    changed table to horizontal cards

--- client/src/components/Orders/index.js
+++ client/src/components/Orders/index.js
@@ -9,42 +9,27 @@ const Orders = (props) => {
     <div>
       <OrderNav />
       <div className='container'>
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col">Order Number</th>
-              <th scope="col">Date</th>
-              <th scope="col">Item</th>
-              <th scope="col">Quantity</th>
-              <th scope='col'>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Date 1</td>
-              <td>Item 1</td>
-              <td>1</td>
-              <td>$45</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Date 2</td>
-              <td>Item 2</td>
-              <td>1</td>
-              <td>$30</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Date 3</td>
-              <td>Item 3</td>
-              <td>5</td>
-              <td>$50</td>
-            </tr>
-          </tbody>
-        </table>
+        <h2 className='text-center'>Order History</h2>
+        <br></br>
+        <div className="card mb-3">
+          <div className="row no-gutters">
+            <div className="col-md-4">
+              <img src='/images/dragon.jpg' className="card-img" alt="..." />
+            </div>
+            <div className="col-md-6">
+              <div className="card-body">
+                <h5 className="card-title">Item 1</h5>
+                <p className="card-text">This is an example of a description for a purchased item. This is only a test.</p>
+                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+            <div className="col-md-2">
+            <span className='align-middle'><p className='text-center'>lasjdf</p></span>
+          </div>
+        </div>
       </div>
     </div>
+  </div>
   );
 };
 
