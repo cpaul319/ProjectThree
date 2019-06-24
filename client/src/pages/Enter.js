d8a942572c21467d583989a7fa73555239e78617
alex chalyy <alexchalyy@gmail.com>
2019-06-24 10:30:21 -0400

    log in redirect to sale page is not working

--- client/src/pages/Enter.js
+++ client/src/pages/Enter.js
@@ -10,6 +10,7 @@ import { Button } from 'reactstrap';
 import Axios from 'axios';
 
 class Enter extends Component {
+
     state = {
         email: "",
         password: "",
@@ -39,52 +40,47 @@ class Enter extends Component {
 
     handleFormSubmit = event => {
 
+        //var users = []; // array of users (including username and password), is populated every time user clicks log in button for input validation
+
         const user = {
-            
+
             email: this.state.email,
             password: this.state.password
-           
+
         }
+
         console.log("submit!");
         console.log("email: " + this.state.email);
         console.log("password: " + this.state.password);
-        if (this.state.email && this.ValidateEmail() && this.state.password) {
-            this.setRedirect();
-            this.renderRedirect();
-            /*
-            $.ajax({
-                method: "GET",
-                url: "/api/allusers"
-              })
-                // With that done, add the note information to the page
-                .then(function (data) {
-                  console.log("those are the contents of the table");
-                  console.log(data);
-                });*/
-                Axios.get('api/allusers', user)
-                .then(function (response) {
-                    console.log("this is db results");
-                    console.log(response);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-            }
-        //     Axios.get('/api/allusers').then(req,res);
-        //     console.log("this is db results");
-        //     console.log(res);
 
-        // } else {
-        //     console.log("e-mail empty");
-        // }
+        Axios.get('api/allusers', user)
+            .then(function (response) {
+                console.log("this is db results");
+                console.log(response);
+
+                for (var c = 0; c < response.data.length; c++) {
+                    if (user.email == response.data[c].email && user.password == response.data[c].password) {
+                        console.log("user exists in db!");
+                        LogIn();
+                    }
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    LogIn() {
+        this.setRedirect();
+        this.renderRedirect();
     }
 
     ValidateEmail() {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        
+
         return re.test(String(this.state.email).toLowerCase());
     }
-  
+
     render() {
         return (
             <div className="App">
@@ -92,9 +88,9 @@ class Enter extends Component {
                 <p>Login page</p>
                 {this.renderRedirect()}
                 <AvForm>
-                    <AvField name="email" label="Email" type="email" onChange={this.handleInputChange} validate={{ 
-                        email: {value: true, errorMessage: 'Please enter valid e-mail'},
-                        required: {value: true, errorMessage: 'Please enter e-mail'} 
+                    <AvField name="email" label="Email" type="email" onChange={this.handleInputChange} validate={{
+                        email: { value: true, errorMessage: 'Please enter valid e-mail' },
+                        required: { value: true, errorMessage: 'Please enter e-mail' }
                     }} />
                     <AvField name="password" label="Password" type="text" onChange={this.handleInputChange} validate={{
                         required: { value: true, errorMessage: 'Please enter password' },
