07f6e74d4a308e514dfe68d9e9e63573b69b7f01
unknown <kevhemsoth@gmail.com>
2019-06-25 20:19:42 -0400

    added new order history cards

--- client/src/components/Orders/index.js
+++ client/src/components/Orders/index.js
@@ -1,8 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
-import OrderNav from '../OrdrNav';
->>>>>>> d16bcc6e373530c21cef77a8188abb19e96b6900
 import './orders.css';
 import OrderNav from '../OrderNav'
 
@@ -13,7 +9,6 @@ const Orders = (props) => {
     <div>
       <OrderNav />
       <div className='container'>
-<<<<<<< HEAD
         <h2 className='text-center'>Order History</h2>
         <br></br>
         <div className="card mb-3">
@@ -25,15 +20,47 @@ const Orders = (props) => {
               <div className="card-body">
                 <h5 className="card-title">Item 1</h5>
                 <p className="card-text">This is an example of a description for a purchased item. This is only a test.</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
               </div>
             </div>
-            <div className="col-md-2">
-            <span className='align-middle'><p className='text-center'>lasjdf</p></span>
+            <div className="col-md-2 row align-items-center justify-content-center">
+            <p>$50</p>
           </div>
         </div>
-=======
-        <table className="table table-striped">
+      </div>
+      <div className="card mb-3">
+          <div className="row no-gutters">
+            <div className="col-md-4">
+              <img src='/images/gotmap.jpg' className="card-img" alt="..." />
+            </div>
+            <div className="col-md-6">
+              <div className="card-body">
+                <h5 className="card-title">Item 2</h5>
+                <p className="card-text">This is an example of a description for a purchased item. This is only a test.</p>
+              </div>
+            </div>
+            <div className="col-md-2 row align-items-center justify-content-center">
+            <p>$30</p>
+          </div>
+        </div>
+      </div>
+      <div className="card mb-3">
+          <div className="row no-gutters">
+            <div className="col-md-4">
+              <img src='/images/thrones1.jpg' className="card-img" alt="..." />
+            </div>
+            <div className="col-md-6">
+              <div className="card-body">
+                <h5 className="card-title">Item 3</h5>
+                <p className="card-text">This is an example of a description for a purchased item. This is only a test.</p>
+              </div>
+            </div>
+            <div className="col-md-2 row align-items-center justify-content-center">
+            <p>$15</p>
+          </div>
+        </div>
+      </div>
+
+       {/* <table className="table table-striped">
           <thead>
             <tr>
               <th scope="col">Order Number</th>
@@ -58,9 +85,7 @@ const Orders = (props) => {
               <td>$50</td>
             </tr>
           </tbody>
-        </table>
->>>>>>> d16bcc6e373530c21cef77a8188abb19e96b6900
-      </div>
+  </table>*/}
     </div>
   </div>
   );
