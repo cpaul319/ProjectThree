71091dfc67b5b7b84355f364839b1e18db11c862
Igor Guba <gubaigor@gmail.com>
2019-06-26 21:47:55 -0400

    sale navbar styling, links-buttons

--- client/src/components/SaleNav/index.js
+++ client/src/components/SaleNav/index.js
@@ -5,11 +5,11 @@ import "./style.css";
 function SaleNav() {
   return (
     <header>
-    <nav className="navbar navbar-dark bg-dark">
-      <Link to="*">Hello</Link> &nbsp; | &nbsp;
-      <Link to="/account">Account</Link> &nbsp; | &nbsp;
-      <Link to="/orders">Orders</Link> &nbsp; | &nbsp;
-      <Link to="/">Log Out</Link> &nbsp; | &nbsp;
+      <nav className="navbar navbar-dark bg-dark">
+        <Link className="text-light sale-nav-link" to="*">Hello</Link> &nbsp; | &nbsp;
+      <Link className="text-light sale-nav-link" to="/account">Account</Link> &nbsp; | &nbsp;
+      <Link className="text-light sale-nav-link" to="/orders">Orders</Link> &nbsp; | &nbsp;
+      <Link className="text-light sale-nav-link" to="/">Log Out</Link> &nbsp; | &nbsp;
     </nav>
     </header>
   );
