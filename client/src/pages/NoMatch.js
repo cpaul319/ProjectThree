c8710fe5884e1bd89706aca798e1d357c4c1aac0
alex chalyy <alexchalyy@gmail.com>
2019-07-06 20:02:07 -0400

    code clean up

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,21 +1,16 @@
 
+//  This page is opened if non-existing page is opened.
+
 import React, { Component } from "react";
-import ReactDOM from 'react-dom';
-import { isAbsolute } from "path";
-import OrderNav from "../components/OrdrNav";
 import "../App.css"
 
-
-console.log("before Log In function is called.");
 function NoMatch() {
     return (
         <div className="NotFound">
-         
             <p>Not Found</p>
         </div>
     );
 }
-console.log("after Log In function is called.");
 
 export default NoMatch;
 
