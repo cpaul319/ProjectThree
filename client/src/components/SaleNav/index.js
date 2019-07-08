db6eb94e90bbdf9bd342b4941515f94f6a3b839f
David Lawrence <dalaw85@gmail.com>
2019-07-08 19:05:27 -0400

    sale page remove our swag

--- client/src/components/SaleNav/index.js
+++ client/src/components/SaleNav/index.js
@@ -53,7 +53,7 @@ class SaleNav extends Component {
   render() {
     return (
       <header>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light" id="sale-nav">
           <a className="navbar-brand" href="/">GoT Swag</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
